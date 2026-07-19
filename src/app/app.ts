aec0a944958237d09698943f1ddc99e11c76100e
Bismor <cwat2011@mial.ru>
2026-07-20 01:15:51 +0300

    feat: перевод блоков на отдельные компоненты

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { Services } from './components/services/services';
+import { Destination } from './components/destination/destination';
+import { Booking } from './components/booking/booking';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Hero, Services, Destination, Booking],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/components/destination/destination.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination',
+  imports: [],
+  templateUrl: './destination.html',
+  styleUrl: './destination.scss',
+})
+export class Destination {}
