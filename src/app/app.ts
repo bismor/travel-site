13591ded3a9916dbcf727f9b78ac4d801dbbcfd3
Bismor <cwat2011@mial.ru>
2026-07-20 08:38:13 +0300

    feat: создание компонентов для о нас и подписки

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,11 @@ import { Hero } from './components/hero/hero';
 import { Services } from './components/services/services';
 import { Destination } from './components/destination/destination';
 import { Booking } from './components/booking/booking';
+import { About } from './components/about/about';
+import { Subscribe } from './components/subscribe/subscribe';
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, Services, Destination, Booking],
+  imports: [Header, Hero, Services, Destination, Booking, About, Subscribe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
